90a94c17ac85c8a51ea1a66fd3671eb0f7454549
Rik Wassens <me@rikwassens.com>
2018-06-25 20:05:00 +0200

    Rotating CSE
    The searcher/scraper can now rotate between three different CSEs - with more added easily - to prevent rate limiting.

--- scraper.py
+++ scraper.py
@@ -6,10 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import unicodecsv as csv
+import random
 import time
 import json
 import sys
 import os
+
 def scrape(html):
 	soup = BeautifulSoup(html, 'html.parser')
 	results = soup.findAll('div', {'class' : 'gsc-webResult gsc-result'})
@@ -26,34 +28,47 @@ def scrape(html):
 
 	return urls
 
-cse_url = 'https://cse.google.com/cse/publicurl?cx=006674705950714944870:ejjevixkyr4'
+cse_urls = {	'https://cse.google.com/cse/publicurl?cx=006674705950714944870:oubvsfffxwo' : False,
+				'https://cse.google.com/cse/publicurl?cx=006674705950714944870:ejjevixkyr4' : False,
+				'https://cse.google.com/cse/publicurl?cx=006674705950714944870:pksdgtyzbtg' : False
+			}
+
 
 with open('updated_handles.csv') as csv_in:
 	reader = csv.reader(csv_in)
 	data = [line for line in reader]
 
-driver = webdriver.Firefox(executable_path='./geckodriver')
-driver.set_window_size(640, 640)
-driver.get(cse_url)
 
-if 'Google' in driver.title:
-	print '[i] Succesfully initiated browser.'
-else:
-	print '[!] Error initiating browser. Quitting.'
-	sys.exit()
+def build_driver(cse_url):
+	driver = webdriver.Firefox(executable_path='./geckodriver')
+	driver.set_window_size(640, 640)
+	driver.get(cse_url)
 
-inputs = driver.find_elements_by_tag_name('input')
+	if 'Google' in driver.title:
+		print '[i] Succesfully initiated browser.'
+	else:
+		print '[!] Error initiating browser. Quitting.'
+		sys.exit()
 
-for i in inputs:
-	if i.get_attribute('class') == 'gsc-input':
-		query_box = i
+	inputs = driver.find_elements_by_tag_name('input')
 
-button = driver.find_elements_by_tag_name('button')
+	for i in inputs:
+		if i.get_attribute('class') == 'gsc-input':
+			query_box = i
 
-for b in button:
-	if b.get_attribute('class') == 'gsc-search-button gsc-search-button-v2':
-		submit_button = b
+	buttons = driver.find_elements_by_tag_name('button')
 
+	for button in buttons:
+		if button.get_attribute('class') == 'gsc-search-button gsc-search-button-v2':
+			submit_button = button
+
+	return driver, submit_button, query_box, cse_url
+
+
+current_cse = random.choice(cse_urls.keys())
+cse_urls[current_cse] = True
+driver, submit_button, query_box, current_cse = build_driver(current_cse)
+	
 for troll_handle in data:
 	if 'troll.json' in os.listdir('.'):
 		with open('troll.json', 'r') as json_in:
@@ -69,16 +84,25 @@ for troll_handle in data:
 		out = {'Trolls' : []}
 
 	t0 = time.time()
+	
 	print '[i] Looking for <%s>.' % troll_handle[0]
 	query_box.send_keys('"%s"' % troll_handle[0])
 	query_box.send_keys(Keys.RETURN)
-	submit_button.click()
+#	submit_button.click()
 	
 	# wait for overlay to be visible
 	try:
 		WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'gsc-results-wrapper-visible')))
 	except TimeoutException:
 		print '\t[!] Timed-out while waiting for form to be submitted...'
+		
+		# time-out: get other CSE
+		driver.close()
+		cse_urls[current_cse] = False
+		new_cse = random.choice([cse for cse in cse_urls.keys() if cse_urls[cse] != True])
+		cse_urls[new_cse] = True
+		print '\t[i] Starting up new CSE: <%s>' % new_cse
+		driver, submit_button, query_box, current_cse = build_driver(new_cse)
 		query_box.clear()
 		continue
 
@@ -105,7 +129,7 @@ for troll_handle in data:
 		json.dump(out, json_out, sort_keys=True, indent=4)
 
 	query_box.clear()
-	time.sleep(time.time() - t0)
+	time.sleep((time.time() - t0) * 5)
 
 driver.close()
 sys.exit()
